592ae872f268d2e9d47aa56155e4f41c80bb163a
Velizar <velizar@next-dc.com>
2025-05-06 12:52:26 +0300

    Files paths on Build

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { sync } from "glob";
 export default {
   appType: "mpa",
   root: "./src",
+  base: './',
   build: {
-    outDir: "../dist",
+    outDir: "../dist", 
     emptyOutDir: true,
     rollupOptions: {
-      input: sync("./src/**/*.html".replace(/\\/g, "/")),
+      input: sync("./src/**/*.html".replace(/\\/g, "./")),
     },
   }
 };
